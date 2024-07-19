85ae6eea777e9acbb3353e931012c311e5ec5f00
kelvin <kahwaleung@gmail.com>
2024-07-19 16:15:41 +0900

    bug fixed

--- sake_app/urls.py
+++ sake_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.sake_list, name='sake_list'),  # 客戶展示頁面
@@ -8,3 +10,6 @@ urlpatterns = [
     path('update/<int:pk>/', views.sake_update, name='sake_update'),
     path('delete/<int:pk>/', views.sake_delete, name='sake_delete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
